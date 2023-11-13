2a2ba93bfdb679c210355345a81a6fd477f7b2a0
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2023-11-13 18:34:02 +0900

    feat :결과 더미데이터 제작 후 더미데이터로 출력

--- src/components/form/ResultForm.js
+++ src/components/form/ResultForm.js
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
-
+import resultData from '../../assets/dummy_data/resultData.json';
 function ResultForm() {
   const [allergenData, setAllergenData] = useState([]);
   const username = '한상우';
 
   useEffect(() => {
-    const headers = {
-      Authorization: 'Bearer YOUR_ACCESS_TOKEN',
-      'Content-Type': 'application/json',
-    };
-
-    axios
-      .get('http://localhost:3000/api/search', { headers, params: { q: '{}' } })
-      .then((response) => {
-        setAllergenData(response.data.result);
-      })
-      .catch((error) => {
-        console.error('API 호출 오류:', error);
-      });
-  }, []);
+    setAllergenData(resultData.result);
+  }, [resultData]);
 
   return (
     <div>
@@ -32,15 +19,15 @@ function ResultForm() {
               <Description isAllergen={true}>
                 [
                 {item.ingredient
-                  .map((ingredient) => `[${ingredient}]`)
+                  .map((ingredient) => `${ingredient}`)
                   .join(', ')}
                 ] 이(가)
               </Description>
             )}
-            <Title>[{username}]님의</Title>
+            <Title>{username}님의</Title>
             {item.cause.length > 0 && (
               <Description isAllergen={true}>
-                [{item.cause.map((causeItem) => `[${causeItem}]`).join(', ')}]
+                [{item.cause.map((causeItem) => `${causeItem}`).join(', ')}]
                 알러지를 유발합니다!
               </Description>
             )}
@@ -62,7 +49,7 @@ const Title = styled.h1`
 `;
 
 const Description = styled.p`
-  font-size: ${({ theme }) => theme.fontsize.TITLE};
+  font-size: ${({ theme }) => theme.fontsize.S_TITLE};
   color: ${(props) => (props.isAllergen ? 'red' : 'black')};
   text-align: center;
   margin-bottom: 15px;

12e16224da51881f4496d29c9a300c6cf8c3151e
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2023-11-16 15:53:54 +0900

    design: 알러지 입력폼 디자인 수정

--- src/pages/AllergyPage.js
+++ src/pages/AllergyPage.js
@@ -1,14 +1,29 @@
 import AllergyForm from '../components/form/AllergyForm';
 import styled from 'styled-components';
+import DefaultImage from '../assets/images/default_img.svg';
 
 const AllergyPage = () => {
   return (
     <>
+      <ImageWrapper>
+        <img src={DefaultImage} alt="Default Image" />
+      </ImageWrapper>
       <Title>알러지가 있는 재료를 선택해주세요</Title>
       <AllergyForm />
     </>
   );
 };
+
+const ImageWrapper = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+
+  img {
+    max-width: 100%;
+    height: auto;
+    max-height: 200px;
+  }
+`;
 
 const Title = styled.p`
   margin-bottom: 30px;
@@ -16,7 +31,7 @@ const Title = styled.p`
   font-size: ${({ theme }) => theme.fontsize.S_TITLE};
   font-weight: ${({ theme }) => theme.fontweight.SEMIBOLD};
   color: ${({ theme }) => theme.colors.MAIN_COLOR};
-  margin-top: 120px;
+  margin-top: 20px;
 `;
 
 export default AllergyPage;
